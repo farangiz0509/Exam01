76c079fd786f6f6fbf5519c020d1d01d2d5f8c7e
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-27 19:03:20 +0545

    done

---
+++ task15.py
@@ -0,0 +1,8 @@
+a = 30
+
+if a % 2:
+    print("2 ga bolinadi")
+elif a % 3:
+    print("3 ga bolinadi")
+elif a % 5:
+    print("5 ga bolinadi")
---
+++ task16.py
@@ -0,0 +1,15 @@
+narx = 100.000
+yosh = int (input("yoshingizni kiriting"))
+
+if yosh == 5:
+    result = 100.000 * 50 /100
+    print(result )
+if yosh == 8:
+    result = 100.000 * 20/100
+    print(result)
+if yosh > 18:
+    print ("chegirma yoq")
+    
+    
+
+
