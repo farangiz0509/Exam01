886749a587977030e993e78f3d9780bd923833dc
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-27 18:40:20 +0545

    done

---
+++ task14.py
@@ -0,0 +1,9 @@
+a = "report.pdf"
+types = ".pdf", ".docx",  ".txt "
+
+if a.endswith ("pdf"):
+    print("pdf")
+elif a.endswith ("docx"):
+    print("docx")
+elif a.endswith ("txt"):
+    print("txt")
